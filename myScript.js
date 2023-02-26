411f54db2c6576bb107ef5e631dd90bff5beff81
w144mjb <60976610+w144mjb@users.noreply.github.com>
2023-02-26 13:15:32 -0500

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,27 @@
+function myFunction0(ch, chNum) {
+	document.getElementById("ch0").style.backgroundColor = "gray";
+	document.getElementById("ch1").style.backgroundColor = "darkgray";
+	document.getElementById("ch2").style.backgroundColor = "darkgray";
+	document.getElementById("ch3").style.backgroundColor = "darygray";
+}
+
+function myFunction1(ch, chNum) {
+	document.getElementById("ch0").style.backgroundColor = "darkgray";
+	document.getElementById("ch1").style.backgroundColor = "gray";
+	document.getElementById("ch2").style.backgroundColor = "darygray";
+	document.getElementById("ch3").style.backgroundColor = "darygray";
+}
+
+function myFunction2(ch, chNum) {
+	document.getElementById("ch0").style.backgroundColor = "darkgray";
+	document.getElementById("ch1").style.backgroundColor = "darkgray";
+	document.getElementById("ch2").style.backgroundColor = "gray";
+	document.getElementById("ch3").style.backgroundColor = "darygray";
+}
+
+function myFunction3(ch, chNum) {
+	document.getElementById("ch0").style.backgroundColor = "darkgray";
+	document.getElementById("ch1").style.backgroundColor = "darkgray";
+	document.getElementById("ch2").style.backgroundColor = "darygray";
+	document.getElementById("ch3").style.backgroundColor = "gray";
+}
