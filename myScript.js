cafbf213cbcdca0cae28f7a3291c5badebc6e317
w144mjb <60976610+w144mjb@users.noreply.github.com>
2023-02-26 22:03:44 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,26 +2,26 @@ function myFunction0(ch, chNum) {
 	document.getElementById("ch0").style.backgroundColor = "gray";
 	document.getElementById("ch1").style.backgroundColor = "darkgray";
 	document.getElementById("ch2").style.backgroundColor = "darkgray";
-	document.getElementById("ch3").style.backgroundColor = "darygray";
+	document.getElementById("ch3").style.backgroundColor = "darkgray";
 }
 
 function myFunction1(ch, chNum) {
 	document.getElementById("ch0").style.backgroundColor = "darkgray";
 	document.getElementById("ch1").style.backgroundColor = "gray";
-	document.getElementById("ch2").style.backgroundColor = "darygray";
-	document.getElementById("ch3").style.backgroundColor = "darygray";
+	document.getElementById("ch2").style.backgroundColor = "darkgray";
+	document.getElementById("ch3").style.backgroundColor = "darkgray";
 }
 
 function myFunction2(ch, chNum) {
 	document.getElementById("ch0").style.backgroundColor = "darkgray";
 	document.getElementById("ch1").style.backgroundColor = "darkgray";
 	document.getElementById("ch2").style.backgroundColor = "gray";
-	document.getElementById("ch3").style.backgroundColor = "darygray";
+	document.getElementById("ch3").style.backgroundColor = "darkgray";
 }
 
 function myFunction3(ch, chNum) {
 	document.getElementById("ch0").style.backgroundColor = "darkgray";
 	document.getElementById("ch1").style.backgroundColor = "darkgray";
-	document.getElementById("ch2").style.backgroundColor = "darygray";
+	document.getElementById("ch2").style.backgroundColor = "darkgray";
 	document.getElementById("ch3").style.backgroundColor = "gray";
 }
